86c0e7ded6f006ab9648495829b56039af70830f
Zack Munich <zack.munich@gmail.com>
2015-09-24 18:01:34 -0400

    put command output to strings

--- ip.py
+++ ip.py
@@ -1,4 +1,10 @@
 import os
-os.system("curl https://api.ipify.org")
-print
-os.system("nmap -sn 10.0.0.2/24")
+p = os.popen('curl https://api.ipify.org',"r")
+while 1:
+    externalIP = p.readline()
+    if not externalIP: break
+p = os.popen('nmap -sn 10.0.0.2/24')
+while 1:
+    localIPs = p.readline()
+    if not localIPs: break
+print(localIPs)

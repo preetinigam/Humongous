4e4720cea06ae9d735fe833088b6a2b15fff2edb
Zack Munich <zack.munich@gmail.com>
2015-09-26 15:39:53 -0400

    fixed tabs because vim and the python interpreter do not agree what a tab is

--- ip.py
+++ ip.py
@@ -19,10 +19,10 @@ print(externalIP)
 print(localIPs)
 while(len(localIPs) > x):
     if(localIPs[x].isdigit() or localIPs[x] == '.'):
-	curIP = curIP + localIPs[x]
+        curIP = curIP + localIPs[x]
     elif(localIPs[x] == '\n' and len(curIP) > 0 and curIP != '10.0.0.1' and (curIP[0] == '1' and curIP[1] == '0')):
-	IPlist.append(curIP)
-	curIP = ''
-	numberOfIPs = numberOfIPs + 1
+        IPlist.append(curIP)
+        curIP = ''
+        numberOfIPs = numberOfIPs + 1
     x = x + 1
 print(IPlist)

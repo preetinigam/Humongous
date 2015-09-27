160ab6fcfa6e73e7234897b3d770cad121e15b4c
Zack Munich <zack.munich@gmail.com>
2015-09-27 03:30:04 -0400

    fixed string parsing error added some comments

--- ip.py
+++ ip.py
@@ -5,12 +5,12 @@ curIP = ''
 x = 0
 numberOfIPs = 0
 IPlist = []
-p = os.popen('curl https://api.ipify.org')
+p = os.popen('curl https://api.ipify.org')"""this transfers data from that address, the address contains our external IP in plain text"""
 while 1:
     IP = p.readline()
     if not IP: break
     externalIP = externalIP + IP
-p = os.popen('nmap -sn 10.0.0.2/24')
+p = os.popen('nmap -sn 10.0.0.1/24')"""this scans the LAN to find all IP addresses connected to the router"""
 while 1:
     IP = p.readline()
     if not IP: break
@@ -18,11 +18,13 @@ while 1:
 print(externalIP)
 print(localIPs)
 while(len(localIPs) > x):
-    if(localIPs[x].isdigit() or localIPs[x] == '.'):
+    if(localIPs[x].isdigit() or localIPs[x] == '.'):"""moves all the digits and periods to a the current possible IP"""
         curIP = curIP + localIPs[x]
-    elif(localIPs[x] == '\n' and len(curIP) > 0 and curIP != '10.0.0.1' and (curIP[0] == '1' and curIP[1] == '0')):
+    elif(len(curIP) > 0 and curIP != '10.0.0.1' and (curIP[0] == '1' and curIP[1] == '0')):"""makes sure that the digit is an IP and isn't the routers IP"""
         IPlist.append(curIP)
         curIP = ''
         numberOfIPs = numberOfIPs + 1
+    elif(localIPs[x] == '\n'):""""This will reset the current IP string if a new line happens"""
+        curIP = ''
     x = x + 1
 print(IPlist)
